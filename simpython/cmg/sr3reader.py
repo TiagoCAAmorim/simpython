bc9e513679978d1e7d94b3a1263402191d761f6c
TiagoCAAmorim <tiago.ca.amorim@gmail.com>
2024-03-13 14:01:47 -0300

    Units manipulation

--- simpython/cmg/sr3reader.py
+++ simpython/cmg/sr3reader.py
@@ -37,17 +37,15 @@ class Sr3Reader:
         Writes data into a grid file.
     """
 
-    def __init__(self, file_path):
+    def __init__(self, file_path, include_additional_units=True):
         """
         Parameters
         ----------
         file_path : str
             Path to grid file.
-        encoding : str, optional
-            Encoding used for reading and writting files.
-            (default: 'utf-8')
-        auto_read : bool, optional
-            Defines if should try to read input file.
+        include_additional_units : bool, optional
+            Defines if some additional units
+            should be included.
             (default: True)
         """
 
@@ -64,6 +62,17 @@ class Sr3Reader:
 
 
         self.read()
+
+        if include_additional_units:
+            additional_units = [
+                ('m3', 'MMm3', (1.e-6, 0.0)),
+                ('bbl', 'MMbbl', (1.e-6, 0.0)),
+                ('kg/cm2', 'kgf/cm2', (1., 0.0)),
+            ]
+
+            for old, new, (gain, offset) in additional_units:
+                self.add_new_unit(old, new, gain, offset)
+            self.set_current_unit('pressure','kgf/cm2')
 
     def list_hdf(self):
         """Returns dict with all groups and databases in file"""
@@ -185,3 +194,183 @@ class Sr3Reader:
                         'long name':long_name,
                         'dimensionality_string':dimensionality}
         _ = self._master_property_list.pop('')
+
+    def _unit_from_dimensionality(self, dimensionality_string):
+        if dimensionality_string == '':
+            return ''
+        unit = ''
+        if dimensionality_string[0] == '-':
+            unit = '1'
+        d = ''
+        for c in dimensionality_string:
+            if c == '|':
+                unit = unit + self._unit_list[int(d)]['current']
+                d = ''
+            elif c == '-':
+                unit = unit + '/'
+            else:
+                d = d + c
+        return unit
+
+    def _unit_conversion_from_dimensionality(self, dimensionality_string, is_delta=False):
+        if dimensionality_string == '':
+            return (1., 0.)
+        gain = 1.
+        offset = 0.
+        inverse = False
+        if dimensionality_string[0] == '-':
+            inverse = True
+        d = ''
+        for c in dimensionality_string:
+            if c == '|':
+                unit = self._unit_list[int(d)]['current']
+                gain_new, offset_new = self._unit_list[int(d)]['conversion'][unit]
+                d = ''
+                if inverse:
+                    gain = gain / gain_new
+                    offset = 0.
+                else:
+                    gain = gain * gain_new
+                    if is_delta:
+                        offset = 0.
+                    else:
+                        offset = offset * gain_new + offset_new
+            elif c == '-':
+                inverse = True
+            else:
+                d = d + c
+        return (gain, offset)
+
+    def _update_properties_units(self):
+        for p in self._master_property_list.values():
+            p['conversion'] = self._unit_conversion_from_dimensionality(
+                p['dimensionality_string'])
+            p['unit'] = self._unit_from_dimensionality(
+                p['dimensionality_string'])
+
+    def _get_unit_type_number(self, dimensionality):
+        if isinstance(dimensionality, str):
+            d = [d for d in self._unit_list
+                 if self._unit_list[d]['type'] == dimensionality.lower()]
+            if len(d) == 0:
+                raise ValueError(f'{dimensionality} is not a valid unit type.')
+            if len(d) > 1:
+                raise ValueError(f'{dimensionality} found more than once. Check code!')
+            return d[0]
+        if isinstance(dimensionality, int):
+            return dimensionality
+        msg = f'Valid types for dimensionality are str and int. Got {type(dimensionality)}.'
+        raise TypeError(msg)
+
+    def set_current_unit(self, dimensionality, unit):
+        """Sets the current unit for a given dimensionality.
+
+        Parameters
+        ----------
+        dimensionality : str or int
+            Dimensionality to modify.
+        unit : str
+            New unit for the given dimensionality.
+
+        Raises
+        ------
+        ValueError
+            If an invalid dimensionality is provided.
+        ValueError
+            If an invalid unit is provided.
+        TypeError
+            If an invalid dimensionality variable
+            type is provided.
+        """
+        dimensionality = self._get_unit_type_number(dimensionality)
+        if unit not in self._unit_list[dimensionality]['conversion']:
+            msg = f'{unit} is not a valid unit for {self._unit_list[dimensionality]['type']}.'
+            raise ValueError(msg)
+        self._unit_list[dimensionality]['current'] = unit
+        self._update_properties_units()
+
+    def get_current_units(self):
+        """Returns dict with current units.
+
+        Parameters
+        ----------
+        None
+        """
+        current_units = {}
+        for d in self._unit_list.values():
+            current_units[d["type"]] = d["current"]
+
+    def _get_unit_numbers(self, unit):
+        out = []
+        for u in self._unit_list:
+            for c in self._unit_list[u]['conversion']:
+                if unit == c:
+                    out.append(u)
+        return out
+
+    def add_new_unit(self, old_unit, new_unit, gain, offset):
+        """Adds a new unit in the form:
+        [new_unit] = [old_unit] * gain + offset
+        Existing units will be overwritten.
+
+        Parameters
+        ----------
+        old_unit : str
+            Existing unit.
+        new_unit : str
+            New unit.
+        gain : float
+            Ratio between old and new units.
+        offset : float
+            Offset between old and new units.
+
+        Raises
+        ------
+        ValueError
+            If old_unit is not found.
+        """
+        unit_numbers = self._get_unit_numbers(old_unit)
+        if len(unit_numbers) == 0:
+            raise ValueError(f'{old_unit} was not found in Master Units Table.')
+
+        for u in unit_numbers:
+            g, o = self._unit_list[u]['conversion'][old_unit]
+            self._unit_list[u]['conversion'][new_unit] = (g * gain, o * gain + offset)
+
+    def get_property_description(self, property_name):
+        """Returns dict with property atributes
+
+        Parameters
+        ----------
+        property_name : str
+            Property to be evaluated.
+
+        Raises
+        ------
+        ValueError
+            If property_name is not found.
+        """
+        if property_name not in self._master_property_list:
+            raise ValueError(f'{property_name} was not found in Master Property Table.')
+        p = self._master_property_list[property_name]
+        return {'description': p['name'],
+                'long description': p['long name'],
+                'unit': p['unit']
+                }
+
+    def get_property_unit(self, property_name):
+        """Returns property current unit
+
+        Parameters
+        ----------
+        property_name : str
+            Property to be evaluated.
+
+        Raises
+        ------
+        ValueError
+            If property_name is not found.
+        """
+        if property_name not in self._master_property_list:
+            raise ValueError(f'{property_name} was not found in Master Property Table.')
+        return self._master_property_list[property_name]['unit']
